5e324b2f4f2f530066a95264f5632ff47e2c7c6b
xietingxian <403007758@qq.com>
2025-11-29 15:32:10 +0800

    Project source code

---
+++ IT8625-WatchDog/WinRing0_Test/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WinRing0_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chip_name = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fan_ram = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cpu_tem = new System.Windows.Forms.Label();
+            this.mod = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.HddTemp = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.chkAutoStart = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(240, 172);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "driver init ...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 224);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "当前芯片型号：";
+            // 
+            // chip_name
+            // 
+            this.chip_name.AutoSize = true;
+            this.chip_name.Location = new System.Drawing.Point(222, 224);
+            this.chip_name.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.chip_name.Name = "chip_name";
+            this.chip_name.Size = new System.Drawing.Size(82, 24);
+            this.chip_name.TabIndex = 2;
+            this.chip_name.Text = "unknow";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 456);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(478, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPU风扇转速[如被动散热或没有接则为10]：";
+            // 
+            // fan_ram
+            // 
+            this.fan_ram.AutoSize = true;
+            this.fan_ram.Location = new System.Drawing.Point(516, 456);
+            this.fan_ram.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.fan_ram.Name = "fan_ram";
+            this.fan_ram.Size = new System.Drawing.Size(82, 24);
+            this.fan_ram.TabIndex = 4;
+            this.fan_ram.Text = "unknow";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 174);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SuperIO_DERVICE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 508);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(490, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "本程序启动后默认加载看门狗，并自动最小化";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(290, 44);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(260, 48);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "专用看门狗";
+            // 
+            // cpu_tem
+            // 
+            this.cpu_tem.AutoSize = true;
+            this.cpu_tem.Location = new System.Drawing.Point(196, 282);
+            this.cpu_tem.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.cpu_tem.Name = "cpu_tem";
+            this.cpu_tem.Size = new System.Drawing.Size(82, 24);
+            this.cpu_tem.TabIndex = 11;
+            this.cpu_tem.Text = "unknow";
+            // 
+            // mod
+            // 
+            this.mod.AutoSize = true;
+            this.mod.Location = new System.Drawing.Point(196, 338);
+            this.mod.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.mod.Name = "mod";
+            this.mod.Size = new System.Drawing.Size(82, 24);
+            this.mod.TabIndex = 12;
+            this.mod.Text = "unknow";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 282);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "CPU温度：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(42, 338);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "主板温度：";
+            // 
+            // HddTemp
+            // 
+            this.HddTemp.AutoSize = true;
+            this.HddTemp.Location = new System.Drawing.Point(196, 398);
+            this.HddTemp.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.HddTemp.Name = "HddTemp";
+            this.HddTemp.Size = new System.Drawing.Size(82, 24);
+            this.HddTemp.TabIndex = 15;
+            this.HddTemp.Text = "unknow";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(46, 396);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 24);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "硬盘温度：";
+            // 
+            // chkAutoStart
+            // 
+            this.chkAutoStart.AutoSize = true;
+            this.chkAutoStart.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.chkAutoStart.Location = new System.Drawing.Point(474, 122);
+            this.chkAutoStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.chkAutoStart.Name = "chkAutoStart";
+            this.chkAutoStart.Size = new System.Drawing.Size(239, 37);
+            this.chkAutoStart.TabIndex = 17;
+            this.chkAutoStart.Text = "开机自动启动";
+            this.chkAutoStart.UseVisualStyleBackColor = true;
+            this.chkAutoStart.CheckedChanged += new System.EventHandler(this.chkAutoStart_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 576);
+            this.Controls.Add(this.chkAutoStart);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.HddTemp);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.mod);
+            this.Controls.Add(this.cpu_tem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fan_ram);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chip_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Aiotoia_Wacthdog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label chip_name;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label fan_ram;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label cpu_tem;
+        private System.Windows.Forms.Label mod;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label HddTemp;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox chkAutoStart;
+    }
+}
+
---
+++ IT8625-WatchDog/WinRing0_Test/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections; // 添加Queue所需命名空间
+using System.Management; // 添加此命名空间
+
+namespace WinRing0_Test
+{
+    public partial class Form1 : Form
+    {
+        private TestByWinRing0 testByWinRing0 = null;
+        private static System.Timers.Timer Timer;
+        private NotifyIcon notifyIcon;
+        private int feedCount = 0;
+        private bool hddTemperatureEverShown = false;
+
+        // 温度历史记录用于滤波
+        private Queue<float> cpuTempHistory = new Queue<float>();
+        private Queue<float> mbTempHistory = new Queue<float>();
+        private Queue<float> hddTempHistory = new Queue<float>();
+        private const int FILTER_WINDOW = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+            InitializeAutoStartSetting();// 注册表初始化
+            InitLogSystem();
+            Init_Driver();
+            this.WindowState = FormWindowState.Minimized;
+            this.SizeChanged += MainFormSizeChanged;
+            this.FormClosing += new FormClosingEventHandler(offdog_FormClosing);
+        }
+        private void InitializeAutoStartSetting()
+        {
+            // 检查注册表设置并更新UI
+            chkAutoStart.Checked = RegistryHelper.IsAutoStartEnabled();
+
+            // 添加事件处理
+            chkAutoStart.CheckedChanged += chkAutoStart_CheckedChanged_1;
+        }
+
+
+
+        private void InitializeCustomComponents()
+        {
+            // 初始化通知图标
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.Icon = SystemIcons.Information; // 默认图标
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += NotifyIconClick;
+            this.notifyIcon.Text = "Aiotoia_WatchDog";
+
+            // 设置初始文本
+            cpu_tem.Text = "-- °C";
+            mod.Text = "-- °C";
+            HddTemp.Text = " -- °C";
+            fan_ram.Text = "-- RPM";
+
+            // 设置颜色
+           // HddTemp.ForeColor = Color.DarkMagenta;
+        }
+
+        // 初始化日志系统
+        private void InitLogSystem()
+        {
+            try
+            {
+                string logDir = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "Aiotoia_Watchdog");
+
+                if (!Directory.Exists(logDir))
+                    Directory.CreateDirectory(logDir);
+
+                string logPath = Path.Combine(logDir, "watchdog_log3.txt");
+                File.AppendAllText(logPath, $"===== 看门狗程序启动 {DateTime.Now} =====\n");
+            }
+            catch { /* 忽略日志错误 */ }
+        }
+
+        private void LogEvent(string message, bool isError = false)
+        {
+            try
+            {
+                string logDir = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "Aiotoia_Watchdog");
+                string logPath = Path.Combine(logDir, "watchdog_log3.txt");
+
+                string prefix = isError ? "[ERROR]" : "[INFO]";
+                File.AppendAllText(logPath, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {prefix} {message}\n");
+            }
+            catch { /* 忽略日志错误 */ }
+        }
+
+        private void offdog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                LogEvent("===== 看门狗程序关闭 =====");
+                LogEvent($"总喂狗次数: {feedCount}");
+
+                if (testByWinRing0 != null)
+                {
+                    testByWinRing0.closewatchdog();
+                    LogEvent("看门狗已关闭");
+                }
+
+                if (MessageBox.Show("确定要关闭看门狗程序吗？", "确认关闭",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogEvent($"关闭错误: {ex.Message}", true);
+            }
+            finally
+            {
+                if (notifyIcon != null)
+                    notifyIcon.Dispose();
+            }
+        }
+
+        private void MainFormSizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                if (notifyIcon != null)
+                {
+                    notifyIcon.ShowBalloonTip(1000, "Aiotoia_看门狗程序",
+                        "看门狗已最小化到系统托盘", ToolTipIcon.Info);
+                }
+            }
+        }
+
+        private void NotifyIconClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+        }
+
+        private void Init_Driver()
+        {
+            try
+            {
+                testByWinRing0 = new TestByWinRing0();
+                bool initResult = testByWinRing0.Initialize();
+
+                if (!initResult)
+                {
+                    LogEvent("WinRing0初始化失败", true);
+                    MessageBox.Show("硬件访问初始化失败，请以管理员权限运行程序", "错误",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // 如果是首次运行，设置开机自启动
+                if (!RegistryHelper.IsAutoStartEnabled())
+                {
+                    RegistryHelper.SetAutoStart(true);
+                    chkAutoStart.Checked = true;
+                    LogEvent("已设置开机自启动");
+                }
+
+                LogEvent("WinRing0初始化成功");
+
+                testByWinRing0.InitSuperIO();
+                string chipName = testByWinRing0.GetChipName();
+                LogEvent($"检测到芯片: {chipName}");
+                chip_name.Text = chipName; // 更新芯片名称显示
+
+                testByWinRing0.InitEc();
+                LogEvent("EC初始化完成");
+
+                testByWinRing0.openwatchdog();
+                LogEvent("看门狗已开启");
+
+                // 初始化定时器
+                Timer = new System.Timers.Timer(5000); // 5秒刷新间隔
+                Timer.AutoReset = true;
+                Timer.Elapsed += TimerElapsedHandler;
+                Timer.Start();
+                LogEvent($"定时监控已启动，间隔: {Timer.Interval}ms");
+
+                this.Load += (s, e) =>
+                {
+                    // 延迟100ms确保窗体完全初始化
+                    System.Threading.Thread.Sleep(100);
+                    UpdateFanSpeed();
+                };
+            }
+            catch (Exception ex)
+            {
+                LogEvent($"初始化错误: {ex.Message}", true);
+                MessageBox.Show($"初始化失败: {ex.Message}", "错误",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TimerElapsedHandler(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                // 喂狗操作
+                if (testByWinRing0 != null)
+                {
+                    testByWinRing0.feedwatchdog(null, null);
+                }
+                feedCount++;
+
+                // 保存当前温度值用于日志记录
+                float? lastCpuTemp = null;
+                float? lastMbTemp = null;
+                float? lastHddTemp = null;                
+
+                bool hddAvailable = false;
+
+                // 更新温度和风扇
+                UpdateTemperatures(ref lastCpuTemp, ref lastMbTemp, ref lastHddTemp, ref hddAvailable);
+                int fanRpm = UpdateFanSpeed();
+
+                // 每150秒记录一次状态
+                if (feedCount % 30 == 0)
+                {
+                    string hddText = hddAvailable ? $", 硬盘: {lastHddTemp:F1}°C" : "";
+                    string fanText = fanRpm >= 0 ? $", 风扇: {fanRpm} RPM" : ", 风扇: N/A";
+                    LogEvent($"运行状态 - 喂狗次数: {feedCount}, CPU: {lastCpuTemp:F1}°C, 主板: {lastMbTemp:F1}°C{hddText}{fanText}");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogEvent($"定时操作错误: {ex.Message}", true);
+            }
+        }
+
+        // 温度滤波算法
+        private float GetFilteredTemp(float newTemp, ref Queue<float> history)
+        {
+            history.Enqueue(newTemp);
+            if (history.Count > FILTER_WINDOW)
+            {
+                history.Dequeue();
+            }
+
+            // 计算平均值
+            float sum = 0;
+            foreach (float temp in history)
+            {
+                sum += temp;
+            }
+            return sum / history.Count;
+        }
+
+
+        private void UpdateTemperatures(ref float? lastCpuTemp, ref float? lastMbTemp, ref float? lastHddTemp, ref bool hddAvailable)
+        {
+            try
+            {
+                if (testByWinRing0 == null) return;
+
+                float cpuTemp = testByWinRing0.GetCpuTemperature();
+                float mbTemp = testByWinRing0.GetMotherboardTemperature();
+                float? rawHddTemp = testByWinRing0.GetHddTemperature();
+
+                // 保存温度值用于日志记录
+                lastCpuTemp = cpuTemp;
+                lastMbTemp = mbTemp;
+                lastHddTemp = rawHddTemp;
+                hddAvailable = rawHddTemp.HasValue;
+
+                // 应用滤波
+                float filteredCpu = GetFilteredTemp(cpuTemp, ref cpuTempHistory);
+                float filteredMb = GetFilteredTemp(mbTemp, ref mbTempHistory);
+
+                float filteredHdd = 0;
+                bool hddAvailableUI = hddAvailable;
+
+                if (rawHddTemp.HasValue)
+                {
+                    filteredHdd = GetFilteredTemp(rawHddTemp.Value, ref hddTempHistory);
+                    hddAvailable = true;
+                }
+
+                // 更新UI
+                this.Invoke((MethodInvoker)delegate
+                {
+                    cpu_tem.Text = $" {filteredCpu:F1}°C";
+                    mod.Text = $" {filteredMb:F1}°C";
+
+                    // 温度过高变色
+                    cpu_tem.ForeColor = filteredCpu > 75 ? Color.Red : Color.DarkGreen;
+                    mod.ForeColor = filteredMb > 65 ? Color.Red : Color.DarkGreen;
+
+                    // 更新硬盘温度显示
+                    if (hddAvailableUI)
+                    {
+                        HddTemp.Text = $" {filteredHdd:F1}°C";
+                        HddTemp.ForeColor = filteredHdd > 50 ? Color.Red : Color.DarkGreen;
+
+                    }
+                    else
+                    {
+                        HddTemp.Text = "硬盘温度: N/A";
+                        HddTemp.ForeColor = Color.Gray;
+
+                        // 记录失败原因
+                        if (!hddTemperatureEverShown && feedCount % 10 == 0)
+                        {
+                            LogEvent($"无法读取硬盘温度，请检查: 1.管理员权限 2.硬盘支持");
+                        }
+                    }
+                });
+
+                // 温度安全检查
+                CheckTemperatureSafety(filteredCpu, filteredMb, hddAvailable ? filteredHdd : (float?)null);
+            }
+            catch (Exception ex)
+            {
+                LogEvent($"温度更新失败: {ex.Message}", true);
+            }
+        }
+
+        private int  UpdateFanSpeed()
+        {
+            try
+            {
+                if (testByWinRing0 == null) return-1;
+
+                int rpm = testByWinRing0.GetFanRpm();
+                this.Invoke((MethodInvoker)delegate
+                {
+                    fan_ram.Text = $" {rpm} RPM";
+
+                    // 根据转速变色
+                    if (rpm > 3200) fan_ram.ForeColor = Color.Red;
+                    //else if (rpm > 2000) fan_ram.ForeColor = Color.Orange;
+                    else fan_ram.ForeColor = Color.Green;
+                });
+                return rpm;
+            }
+            catch (Exception ex)
+            {
+                LogEvent($"风扇转速更新失败: {ex.Message}", true);
+                return -1;
+            }
+        }
+
+        private void CheckTemperatureSafety(float cpuTemp, float mbTemp, float? hddTemp)
+        {
+            const float CPU_MAX_TEMP = 90.0f;
+            const float MB_MAX_TEMP = 80.0f;
+            const float HDD_MAX_TEMP = 50.0f;
+
+            if (cpuTemp > CPU_MAX_TEMP)
+            {
+                LogEvent($"CPU过热警告! 当前温度: {cpuTemp}°C", true);
+                this.Invoke((MethodInvoker)delegate {
+                    if (notifyIcon != null)
+                    {
+                        notifyIcon.ShowBalloonTip(3000, "温度警告",
+                            $"CPU温度过高: {cpuTemp}°C", ToolTipIcon.Warning);
+                    }
+                });
+            }
+
+            if (mbTemp > MB_MAX_TEMP)
+            {
+                LogEvent($"主板过热警告! 当前温度: {mbTemp}°C", true);
+                this.Invoke((MethodInvoker)delegate {
+                    if (notifyIcon != null)
+                    {
+                        notifyIcon.ShowBalloonTip(3000, "温度警告",
+                            $"主板温度过高: {mbTemp}°C", ToolTipIcon.Warning);
+                    }
+                });
+            }
+
+            if (hddTemp.HasValue && hddTemp > HDD_MAX_TEMP)
+            {
+                LogEvent($"硬盘过热警告! 当前温度: {hddTemp}°C", true);
+                this.Invoke((MethodInvoker)delegate {
+                    if (notifyIcon != null)
+                    {
+                        notifyIcon.ShowBalloonTip(3000, "温度警告",
+                            $"硬盘温度过高: {hddTemp}°C", ToolTipIcon.Warning);
+                    }
+                });
+            }
+        }
+
+        // 打开日志菜单项
+        private void openLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string logPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "Aiotoia_Watchdog", "watchdog_log3.txt");
+
+                if (File.Exists(logPath))
+                {
+                    Process.Start("notepad.exe", logPath);
+                }
+                else
+                {
+                    MessageBox.Show("日志文件不存在", "信息",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"无法打开日志: {ex.Message}", "错误",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void chkAutoStart_CheckedChanged_1(object sender, EventArgs e)
+        {
+            bool success = RegistryHelper.SetAutoStart(chkAutoStart.Checked);
+
+            if (success)
+            {
+                string status = chkAutoStart.Checked ? "已启用" : "已禁用";
+                LogEvent($"开机自启动{status}");
+
+                //MessageBox.Show($"开机自启动{status}成功", "设置成功",
+                //    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                LogEvent("开机自启动设置失败，请尝试以管理员身份运行", true);
+                MessageBox.Show("设置失败，请尝试以管理员身份运行程序", "错误",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // 恢复之前的状态
+                chkAutoStart.Checked = !chkAutoStart.Checked;
+            }
+        }
+    }
+}
